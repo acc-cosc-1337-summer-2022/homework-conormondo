2c5afb495aa1b381900604ecf80e0b36574dad42
Conor Manning <conor.manning@mondoshop.com>
2022-06-02 20:55:11 -0500

    assignment 2

--- src/examples/02_expressions/06_multi_assign/main.cpp
+++ src/examples/02_expressions/06_multi_assign/main.cpp
@@ -1,6 +1,21 @@
+#include<iostream>
+#include "multi_assign.h"
+
+using std::cin; using std::cout;
 
 int main()
 {
+    int num;
+    cout<<"Enter a number ...";
+    cin>>num;
+
+    int result = multiply_numbers(num);
+
+    cout<<"Result is "<<result<<"\n";
+
+    int num1 = 4;
+    result = multiply_numbers(num1);
+    cout<<"Result is "<<result<<"\n";
 
     return 0;
 }

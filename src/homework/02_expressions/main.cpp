8367d5f13916727062ad9b61ef1074eff0d3560c
Conor Manning <conor.manning@mondoshop.com>
2022-06-11 18:08:51 -0500

    Homework 2

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,15 +1,36 @@
 ﻿//write include statements
-
+#include "hwexpressions.h"
+#include <iostream>
 
 //write namespace using statement for cout
-
+using namespace std;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	double meal_amount;
+	double tip_rate;
+	double tip_amount;
+	double tax_amount;
+	double total;
+
+	cout<<"Enter meal amount "<<"\n";
+	cin>>meal_amount;
+
+	cout<<"Enter tip amount "<<"\n";
+	cin>>tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	total = tip_amount + tax_amount + meal_amount;
+
+	cout<<"Meal Amount: "<<meal_amount<<"\n";
+	cout<<"Sales Tax: "<<tax_amount<<"\n";
+	cout<<"Tip Amount: "<<tip_amount<<"\n";
+	cout<< "Total: " << total<<"\n";
 
 	return 0;
 }
--- test/homework/02_expressions/02_expressions_tests.cpp
+++ test/homework/02_expressions/02_expressions_tests.cpp
@@ -12,3 +12,15 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+TEST_CASE("verify get_sales_tax_amount function") 
+{
+	REQUIRE(get_sales_tax_amount(10) == .675);
+	REQUIRE(get_sales_tax_amount(20) == 1.35);
+}
+
+TEST_CASE("verify get_tip_amount function") 
+{
+	REQUIRE(get_tip_amount(20, .15) == 3);
+	REQUIRE(get_tip_amount(20, .20) == 4);
+}
+

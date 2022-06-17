eb996c9b776d0b94f0a32e5b2bfd0297f33278d0
Conor Manning <conor.manning@mondoshop.com>
2022-06-16 23:52:45 -0500

    Homework 3

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,33 @@
 //write include statement for decisions header
+#include "decisions.h"
 
+using std::string;
 
 //Write code for function(s) code here
+int get_grade_points(string letter_grade){
+
+    int grade_point;
+
+    if (letter_grade == "A" || letter_grade == "a") {
+        grade_point = 4;
+    }
+    else if (letter_grade == "B" || letter_grade == "b") {
+        grade_point = 3;
+    }
+    else if (letter_grade == "C" || letter_grade == "c") {
+        grade_point = 2;
+    }
+    else if (letter_grade == "D" || letter_grade == "d") {
+        grade_point = 1;
+    }
+    else {
+        grade_point = 0;
+    }
+
+    return grade_point;
+}
+
+double calculate_gpa(int credit_hours, double credit_points)
+{
+    return credit_points / credit_hours;
+}
--- src/homework/03_decisions/decisions.h
+++ src/homework/03_decisions/decisions.h
@@ -1,8 +1,10 @@
 //write include required statements
-
+#include<string>
+#include <iostream>
 
 //Write a function(s) prototype  here
 
+int get_grade_points(std::string letter_grade);
 
-
+double calculate_gpa(int credit_hours, double credit_points);
 
